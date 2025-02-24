b35ef98eb52691bfa018efaea3144c1f264b6ea8
Biswajit <paridabiswa2k@gmail.com>
2025-02-25 00:04:21 +0530

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express();
+
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server s rumming on ${process.env.PORT}succesfully`)
+})
