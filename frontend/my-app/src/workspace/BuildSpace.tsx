e5ce896621de92674e63d286c130a6f07ffbc9cd
Biswajit <paridabiswa2k@gmail.com>
2025-03-19 01:39:40 +0530

    adding components

--- frontend/my-app/src/workspace/BuildSpace.tsx
+++ frontend/my-app/src/workspace/BuildSpace.tsx
@@ -2,24 +2,37 @@ import { useState } from "react"
 
 
 function BuildSpace(){
-   const [projects,setProjects] = useState<string[]>([""]);
+   const [projects,setProjects] = useState([{name:"",techStack:"",description:"",}]);
+   const[workexperinces,setWorkexperinces] = useState([{companyName:"",workdescription:""}])
    
 
-   
+   const addWorkexperince = ()=>{
+    setWorkexperinces([...workexperinces,{companyName:"",workdescription:""}])
+   }
 
    const addProjects = ()=>{
-    setProjects([...projects,""]);
+    setProjects([...projects,{name:"",techStack:"",description:"",}]);
    }
 
 
-   
+   const handleWorkexperince =(index:number,field:"companyName" | "workdescription",value:string)=>{
+    const newWorkexperinces = [...workexperinces];
+    (newWorkexperinces[index] as any)[field] = value
+    setWorkexperinces(newWorkexperinces)
+   }
 
-   const handleProjects=(index:number,value:string)=>{
+   const handleProjects=(index:number,field:"name" | "techStack" | "description",value:string)=>{
     const newProjects = [...projects]
-    newProjects[index] = value
-    setProjects(projects)
+    newProjects[index][field] = value
+    setProjects(newProjects)
    }
 
+
+   const handleSubmit=(e:React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    
+
+   }
     return(
         <>
         <style>
@@ -51,7 +64,7 @@ function BuildSpace(){
                 welcome to build space
             </h1>
 
-            <form >
+            <form onSubmit={handleSubmit}>
                <label htmlFor="">Name</label>
                <input type="text" />
                <label htmlFor="">Linkedin URL</label>
@@ -77,16 +90,43 @@ function BuildSpace(){
                 <div key={index}>
                       <label htmlFor="">project-{index+1}</label>
                       <input type="text" 
-                      value={project}
-                      onChange={(e)=>handleProjects(index,e.target.value)}
+                      value={project.name}
+                      onChange={(e)=>handleProjects(index,"name",e.target.value)}
+                      placeholder="project name"
+                      />
+                      <input type="text" 
+                      value={project.techStack}
+                      onChange={(e)=>handleProjects(index,"techStack",e.target.value)}
+                      placeholder="add techstack used"
+                      />
+                      <input type="text" 
+                      value={project.description}
+                      onChange={(e)=>handleProjects(index,"description",e.target.value)}
+                      placeholder="add description "
                       />
                 </div>
                ))}
                <button onClick={addProjects}>Add project</button>
+                <h2>Work Experience</h2>
+                {workexperinces.map((workexperince,index)=>(
+                    <div key={index}>
+                        <label htmlFor="">company-{index+1}</label>
+                        <input type="text"
+                        value={workexperince.companyName}
+                        onChange={(e)=>handleWorkexperince(index,"companyName",e.target.value)} 
+                        placeholder="company name"
+                         />
+                          <input type="text"
+                        value={workexperince.workdescription}
+                        onChange={(e)=>handleWorkexperince(index,"workdescription",e.target.value)}
+                        placeholder="work done within the company"
+                          />
+                    </div>
+                ))}
+                <button onClick={addWorkexperince}>add workexperince</button>
+                <h2>summery</h2>
 
-
-             
-
+                
             </form>
         </div>
 
