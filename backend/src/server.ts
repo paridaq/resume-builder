bdc7398c878b8b86eee92a79a9ee8a8e05ca4a18
Biswajit <paridabiswa2k@gmail.com>
2025-04-06 03:41:31 +0530

    chat backend  commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,7 @@ import resumeRoute from './routes/resumeRoute'
 import fs from 'fs'
 import path from 'path'
 import { dirname } from 'path'
+import { Request,Response } from 'express'
 
 dotenv.config()
 // connectDB();
@@ -20,9 +21,14 @@ app.use(express.urlencoded({extended:true}))
 //app.use('/public',express.static(path.join(__dirname, "dist/controllers/public")));
 app.use('/public', express.static('dist/controllers/public'))
 app.use('/api/build',resumeRoute)
+app.get('/',(req:Request,res:Response)=>{
+    res.send('what')
+})
 
 
 
 app.listen(process.env.PORT,()=>{
     console.log(`server s running on ${process.env.PORT}succesfully`)
 })
+
+module.exports = app;
